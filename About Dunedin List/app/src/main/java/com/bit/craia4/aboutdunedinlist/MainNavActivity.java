3f621f704063ec09522b7bf028b12e208a54ee8b
acraig94 <acraig94@gmail.com>
2016-03-09 22:31:28 +1300

    4.1 Task 1

---
+++ About Dunedin List/app/src/main/java/com/bit/craia4/aboutdunedinlist/MainNavActivity.java
@@ -0,0 +1,68 @@
+package com.bit.craia4.aboutdunedinlist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_nav);
+
+        setUpNavList();
+
+        ListView navListView = (ListView) findViewById(R.id.LV_navScreen);
+        navListView.setOnItemClickListener(new ScreenNavListClickHandler());
+
+    }
+
+    public class ScreenNavListClickHandler implements AdapterView.OnItemClickListener
+    {
+
+        @Override
+        public void onItemClick(AdapterView<?> list, View view, int position, long id)
+        {
+            String clickedItem = (String) list.getItemAtPosition(position).toString();
+            Intent goToIntent;
+
+            switch (clickedItem)
+            {
+                case "Services":
+                    goToIntent = new Intent(MainNavActivity.this, SubActivityServices.class);
+                    break;
+                case "Fun Things To Do":
+                    goToIntent = new Intent(MainNavActivity.this, SubActivityFunThings.class);
+                    break;
+                case "Dining":
+                    goToIntent = new Intent(MainNavActivity.this, SubActivityDining.class);
+                    break;
+                case "Shopping":
+                    goToIntent = new Intent(MainNavActivity.this, SubActivityShopping.class);
+                    break;
+                default:
+                    goToIntent = null;
+                    break;
+            }
+
+            startActivity(goToIntent);
+
+        }
+    }
+
+    public void setUpNavList()
+    {
+        String[] screens = {"Services", "Fun Things To Do", "Dining", "Shopping"};
+        ArrayAdapter<String> navAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, screens);
+
+        ListView NavListView = (ListView) findViewById(R.id.LV_navScreen);
+        NavListView.setAdapter(navAdapter);
+    }
+
+
+}
---
+++ About Dunedin List/app/src/main/java/com/bit/craia4/aboutdunedinlist/SubActivityShopping.java
@@ -0,0 +1,13 @@
+package com.bit.craia4.aboutdunedinlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubActivityShopping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_activity_shopping);
+    }
+}
